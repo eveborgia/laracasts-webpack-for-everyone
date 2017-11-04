f5038a73f6b19cfd4758b9d67c38027a23eaddf0
Iuliana Borgia <eve@poshtone.net>
2017-11-04 12:10:24 -0400

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,36 @@
+var webpack = require('webpack');
+var path = require('path');
+var inProduction = (process.env.NODE_ENV === 'production');
+
+module.exports = {
+	entry: './src/main.js',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js'
+	},
+	module: {
+		rules: [
+		{
+			test: /\.s[ac]ss$/,
+			use: ['style-loader', 'css-loader', 'sass-loader']
+		},
+
+		{
+			test: /\.css$/,
+			use: ['style-loader', 'css-loader']
+		},
+		    { 
+		    	test: /\.js$/, 
+		    	exclude: /node_modules/, 
+		    	loader: "babel-loader"
+		     }
+		]
+	},
+	plugins: []
+};
+
+if (inProduction) {
+	module.exports.plugins.push(
+		new webpack.optimize.UglifyJsPlugin()
+		);
+}
