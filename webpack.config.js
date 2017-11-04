353778619cf1753eff28d22f13be5d1f056927e1
Iuliana Borgia <eve@poshtone.net>
2017-11-04 16:35:57 -0400

    Episode 9

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,35 @@
 var webpack = require('webpack');
 var path = require('path');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
 var inProduction = (process.env.NODE_ENV === 'production');
 
 module.exports = {
-	entry: './src/main.js',
+	entry: {
+		app:[ 
+		'./src/main.js', 
+		'./src/main.scss'
+		]
+	},
 	output: {
 		path: path.resolve(__dirname, './dist'),
-		filename: 'bundle.js'
+		filename: '[name].js'
 	},
 	module: {
 		rules: [
 		{
-			test: /\.s[ac]ss$/,
-			use: ['style-loader', 'css-loader', 'sass-loader']
+			test: /\.s[ac]ss$/, 
+			use: ExtractTextPlugin.extract({
+				use: ['css-loader', 'sass-loader'],
+			
+				fallback: 'style-loader' 
+			}),
 		},
-
 		{
-			test: /\.css$/,
-			use: ['style-loader', 'css-loader']
+			test: /\.(png|jpe?g|gif|svg|eot|ttf|woff|woff2)$/,
+			loader: 'file-loader',
+			options: {
+				name: 'images/[name].[hash].[ext]'
+			}
 		},
 		    { 
 		    	test: /\.js$/, 
@@ -26,7 +38,14 @@ module.exports = {
 		     }
 		]
 	},
-	plugins: []
+	plugins: [
+		new ExtractTextPlugin('[name].css'),
+
+		new webpack.LoaderOptionsPlugin({
+			minimize: inProduction,
+		})
+		  
+	]
 };
 
 if (inProduction) {
